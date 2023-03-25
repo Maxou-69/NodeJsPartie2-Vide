6bcb0a1a08f35560ec6042fcfe8bb0e464cab27f
Maxou <leroymax2006@gmail.com>
2023-03-25 11:10:00 +0100

    make all working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Ce code crée une application Express simple qui utilise Mustache pour le rendu des vues et bodyParser pour lire les données POST des formulaires. La route GET pour la page d'accueil rend la vue 'mon-template' avec un objet contenant la propriété 'MyName' et la valeur "Maxime".
 
 // Importation des modules nécessaires
+const { time, timeStamp } = require('console');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -22,7 +23,7 @@ app.engine('mustache', mustacheExpress()); // Associe Mustache à l'extension de
 
 // Route GET pour la page d'accueil
 app.get('/', function (req, res) {
-
+  res.render('mon_template', { Myname: "Maxime" });
 });
 
 // Démarrage du serveur sur le port spécifié
